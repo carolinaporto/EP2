8aa63901ee6d6c33f427f3bc0d4c165968448597
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2023-11-08 15:21:04 -0300

    Add da opção de desistir

--- Termo.py
+++ Termo.py
@@ -1,11 +1,4 @@
 import random
-#comando para cores: '\033[0;033m7\033[m'
-
-# Colorindo o texto do início do jogo:
-Azul = '\033[0;034mAzul\033[m'
-Amarelo = '\033[0;033mAmarelo\033[m'
-Cinza = '\033[0;037mCinza\033[m'
-tentativas = '\033[0;031m6\033[m'
 
 # Inicio do jogo:
 print(" =========================== \n|                           |\n| Bem-vindo ao Insper Termo |\n|                           |\n ==== Design de Software === ")
