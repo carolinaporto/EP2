2ec719f213c5a69890f291085db7aa65f7d7505c
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2023-11-12 16:37:14 -0300

    Plavras coloridas na introdução

--- Termo.py
+++ Termo.py
@@ -1,9 +1,10 @@
 import random
+from termcolor import colored
 
 # Inicio do jogo:
 print(" =========================== \n|                           |\n| Bem-vindo ao Insper Termo |\n|                           |\n ==== Design de Software === ")
 print("Comandos: desisto")
 
-print(f"\n Regras:\n  - Você tem {tentativas} tentativas para acertar uma palavra aleatória de 5 letras.\n  - A cada tentativa, a palavra testada terá suas letras coloridas conforme:\n    . {Azul}   : a letra está na posição correta;\n    . {Amarelo}: a palavra tem a letra, mas está na posição errada;\n    . {Cinza}: a palavra não tem a letra.\n  - Os acentos são ignorados;\n  - As palavras podem possuir letras repetidas.\n\nSorteando uma palavra...")
+print(f"\n Regras:\n  - Dependendo do número de letras da palavra que você escolher advinhar, você terá n+1 tentativas para acertá-la.\n  - A cada tentativa, a palavra testada terá suas letras coloridas conforme:\n    . {colored('Azul', 'blue', attrs = ['bold'])}   : a letra está na posição correta;\n    . {colored('Amarelo', 'yellow', attrs = ['bold'])}: a palavra tem a letra, mas está na posição errada;\n    . {colored('Cinza', 'grey', attrs = ['bold'])}: a palavra não tem a letra.\n  - Os acentos são ignorados;\n  - As palavras podem possuir letras repetidas.\n\nSorteando uma palavra...")
 
 print("Já tenho uma palavra! Tente adivinhá-la!")
